6d5e104000e2d2c1d13cd198b15b5108df9ebac3
Matt Holland <mholland@ivansinsurance.com>
2021-08-23 20:46:49 -0400

    rclone config parameters

--- chart/restart.bat
+++ chart/restart.bat
@@ -1,2 +1,5 @@
+cmd /c "gcloud container clusters get-credentials the-incredible-machine --zone us-central1-c --project the-incredible-machine-276601"
+cmd /c "gcloud config configurations activate tim"
+cmd /c "gcloud config set account matt@theincrediblemachine.tv"
 helm uninstall tim-plex
 helm install tim-plex .
